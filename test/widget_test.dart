ad92f235534b548f67d0bcf3bca1870312c3b612
Fabio Andrés Enríquez (fandres51) <fabio.and1514@gmail.com>
2023-03-29 20:00:05 -0500

    test error fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:binary_tools/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BinaryTools());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
